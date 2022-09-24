f96d26f25e98a2e2a906f3d13943af807a2f6eb7
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2022-09-24 15:54:04 +0100

    Update streamlit_app.py
    Added emojis to menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ streamlit.title('Menu for Loaded')
 
 streamlit.header('Proper Dirty Fries')
 
-streamlit.text('Pulled Pork, Caremelised Onions, Caynne Pepper and Tabasco Sauce')
-streamlit.text('Chicken, Chicken Skins, Cheddar Cheese and Barbeque Sauce')
-streamlit.text('Tofu, Cabbage, Lettuce and Vegan Sauce')
+streamlit.text('🐖 Pulled Pork, Caremelised Onions, Caynne Pepper and Tabasco Sauce')
+streamlit.text('🍗 Chicken, Chicken Skins, Cheddar Cheese and Barbeque Sauce')
+streamlit.text('🥑 Avocado, Free-From Cheese, Lettuce and Vegan Sauce')
