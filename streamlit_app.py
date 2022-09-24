495e92f996f5ed1bbe5d3eb1673cc31bf8fbcdee
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2022-09-24 15:57:02 +0100

    Update streamlit_app.py
    Added a Duck dish. Corrected spelling error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ streamlit.title('Menu for Loaded')
 
 streamlit.header('Proper Dirty Fries')
 
-streamlit.text('🐖 Pulled Pork, Caremelised Onions, Caynne Pepper and Tabasco Sauce')
+streamlit.text('🐖 Pulled Pork, Caramelised Onions, Cayenne Pepper and Tabasco Sauce')
 streamlit.text('🍗 Chicken, Chicken Skins, Cheddar Cheese and Barbeque Sauce')
 streamlit.text('🥑 Avocado, Free-From Cheese, Lettuce and Vegan Sauce')
+streamlit.text('🦆 Duck, Mushrooms, Bean Sprouts and Hoisin Sauce')
