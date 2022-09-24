e3e5b416f8817feb47f4dd02e3c0b8a6075e50be
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2022-09-24 15:47:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
-streamlit.title('First Snowflake Streamlit project')
+streamlit.title('Menu for Loaded')
+
+streamlit.header('Proper Dirty Fries')
+
+streamlit.text('Pulled Pork, Caremelised Onions, Caynne Pepper and Tabasco Sauce')
+streamlit.text('Chicken, Chicken Skins, Cheddar Cheese and Barbeque Sauce')
+streamlit.text('Tofu, Cabbage, Lettuce and Vegan Sauce')
