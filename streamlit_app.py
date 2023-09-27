6614936e0340af73869e285f355096d53cb92575
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2023-09-27 13:20:08 +0100

    Update streamlit_app.py
    Fruit List

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit
+import pandas as pd
 
+# Streamlit Headings
 streamlit.title('Proper Good Filth Food')
 streamlit.header('Takeaway Menu')
 streamlit.text('🐖🌶️🍟Pulled Pork and Jalapeno Fries')
 streamlit.text('🦆♨️🍟Crispy Duck and Smoky BBQ Fries')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
