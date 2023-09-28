ea89847fb851e61d98bf9058d263b5114c9d0a1f
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2023-09-28 07:43:39 +0100

    Update streamlit_app.py
    Removed JSON line in app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 # API Requests
 streamlit.header("Fruityvice Fruit Advice")
+
+# Enter a Fruit name to get information about it
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text(fruityvice_response.json())
 # Normalize the JSON data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display the Normalized JSON data
