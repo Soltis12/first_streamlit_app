80c6fd9c33397c8ad680ba7fb0519f68d4b0649c
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2023-09-28 10:16:11 +0100

    Update streamlit_app.py
    Try Except for FruityVice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,19 @@ streamlit.dataframe(fruits_to_show)
 
 # API Requests
 streamlit.header("Fruityvice Fruit Advice")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    streamlit.write('The user entered ', fruit_choice)
+except URLError as e:
+  streamlit.error()
 
-# Enter a Fruit name to get information about it
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# Normalize the JSON data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display the Normalized JSON data
-streamlit.dataframe(fruityvice_normalized)
-
+# Snowflake Connection
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
