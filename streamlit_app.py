4b7f83e60c2cd38f2a9492d0de8970d366cc17b5
Jono Pike <43720545+Soltis12@users.noreply.github.com>
2023-09-28 11:06:55 +0100

    Update streamlit_app.py
    FruityVice function added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+# Define FruityVice Function
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
 # Streamlit Headings
 streamlit.title('Proper Good Filth Food')
@@ -31,9 +36,8 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
     streamlit.write('The user entered ', fruit_choice)
 except URLError as e:
   streamlit.error()
